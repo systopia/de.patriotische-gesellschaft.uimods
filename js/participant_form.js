7e202a9579c79a567109d4aa13737f964b04a278
Jens Schuppe <schuppe@systopia.de>
2019-01-11 15:32:58 +0100

    Uncheck the send_receipt checkbox for cancelled event participations.

--- js/participant_form.js
+++ js/participant_form.js
@@ -2,6 +2,7 @@
 | Patriotische Gesellschaft UI Modififications           |
 | Copyright (C) 2017 SYSTOPIA                            |
 | Author: P. Batroff (batroff@systopia.de)               |
+|         J. Schuppe (schuppe@systopia.de)               |
 | http://www.systopia.de/                                |
 +--------------------------------------------------------+
 | This program is released as free software under the    |
@@ -13,8 +14,44 @@
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
 
-cj(document).ready(function () {
-    cj(document).bind("ajaxComplete", function(){
-        cj("#notify").remove();
+(function ($) {
+  $(document).ready(function () {
+    $(document).bind("ajaxComplete", function () {
+      $("#notify").remove();
     });
-});
+
+    // Disable the "send receipt" checkbox when selecting "Cancelled" as
+    // participant status.
+    //
+    // This is ugly, but we can't use $().on('change') here because the SELECT
+    // element has an onchange attribute with a "return" inside. Thus, we
+    // re-define the onchange property and prepend our function call to the
+    // existing code and use the same approach as sendNotification(): Defining a
+    // global function and calling it from the onchange property, but without a
+    // "return".
+    var onchangeAttr = document.getElementById('status_id').onchange;
+    document.getElementById('status_id').onchange = function() {
+      sendReceipt();
+      if (typeof onchangeAttr === 'function') {
+        onchangeAttr();
+      }
+    };
+  });
+})(jQuery);
+
+/**
+ * Disable the "send_receipt" checkbox for some participant statuses.
+ *
+ * This function is called from the "onchange" attribute on the "status_id"
+ * SELECT element, similar to sendNotification().
+ */
+function sendReceipt() {
+  var receiptStatusIds = "4";
+  receiptStatusIds = receiptStatusIds.split(',');
+  if (cj.inArray(cj('select#status_id option:selected').val(), receiptStatusIds) > -1) {
+    cj("#send_receipt").prop('checked', false);
+  }
+  else {
+    cj("#send_receipt").prop('checked', true);
+  }
+}
